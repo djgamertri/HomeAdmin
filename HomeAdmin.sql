22ccbfbd3a4eb48d022351d11d1a8026bbe6228d
djgamertri_ <djgamertri@gmail.com>
2023-05-28 09:45:33 -0500

    Base de datos

--- HomeAdmin.sql
+++ HomeAdmin.sql
@@ -1,17 +1,18 @@
 create database HomeAdmin;
 use HomeAdmin;
+
 -- Tabla Multas
 create table Tax(
     TaxId int unsigned auto_increment primary key,
     TaxValue int unsigned not null,
     TaxYear year not null
 );
+
 -- Procedimiento Registro de multas
 create procedure RegisterTax(
     in RegisterTaxValue int,
     in RegisterTaxYear year
 )
-
 insert into Tax (TaxValue,TaxYear) values (RegisterTaxValue,RegisterTaxYear);
 
 -- Consulta general de las multas
