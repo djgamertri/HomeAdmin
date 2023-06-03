8103b32acc19e61539f63eb5891eb0b7940c0ad0
djgamertri_ <djgamertri@gmail.com>
2023-06-03 10:57:10 -0500

    Creamos las funciones para peticiones a la API

--- FrontEnd/Controller/SendData.php
+++ FrontEnd/Controller/SendData.php
@@ -1,6 +1,6 @@
 <?php
 
-function sendDataPost ($url,$data,){
+function sendDataPost ($url,$data){
     // Convertir los datos en formato JSON
     $jsonData = json_encode($data);
     // Configurar la solicitud cURL
@@ -27,7 +27,7 @@ function sendDataPost ($url,$data,){
  
      // Procesar la respuesta
      if ($httpCode == 200) {
-         
+         return $response;
          // Hacer algo con los datos de respuesta...
      } else {
          // La solicitud no fue exitosa, manejar el error adecuadamente
@@ -66,7 +66,8 @@ function sendDataGet ($url){
 
 }
 
-function sendDataUpdate($url){
+function sendDataUpdate($url,$data){
+    $jsonData = json_encode($data);
     // Configurar la solicitud cURL
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -102,7 +103,8 @@ function sendDataUpdate($url){
 
 }
 
-function sendDataDelete($url){
+function sendDataDelete($url,$data){
+    $jsonData = json_encode($data);
     // Configurar la solicitud cURL
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -136,12 +138,5 @@ function sendDataDelete($url){
     }
 
 }
-// Configurar la solicitud cURL
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DElETE');
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json'
-));
+
 ?>
