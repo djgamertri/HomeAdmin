d245efee3db0a722cc70edd2d299b7e2b4695805
djgamertri <djgamertri@gmail.com>
2023-06-08 11:36:56 -0500

    Controladores de residente

---
+++ FrontEnd/Controller/Resident/RegisterResident.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once "../SendData.php";
+
+$lastPage = $_SERVER['HTTP_REFERER'];
+
+if(!empty($_POST)){
+    if ($_POST["NameResident"]) {
+        $data = json_decode(sendDataPost("http://localhost:3001/api/Resident",$_POST));
+        if($data->NameResident){
+            header("location: $lastPage?complete=true");
+        }else{
+            header("location: $lastPage?complete=false");
+        }
+    }
+}
+
+?>
