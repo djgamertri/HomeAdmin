4e37a12e88348c611b52ed6a337744d2e9421d40
djgamertri <djgamertri@gmail.com>
2023-06-08 10:04:38 -0500

    Vista de Dashboard

---
+++ FrontEnd/View/Dashboard/UserRegister.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://kit.fontawesome.com/6131ecdde6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../Css/Nav.css">
+    <link rel="stylesheet" href="../../Css/Form.css">
+    <title>Registro de Usuarios</title>
+</head>
+<body>
+    <?php include_once("../../Assets/Nav.php") ?>
+    <?php include_once("../../Assets/FormRegistrarUsuario.php") ?>
+</body>
+</html>
