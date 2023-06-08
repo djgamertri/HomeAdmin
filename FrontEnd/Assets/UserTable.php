48779c79936abf6e488b65db3ffe2d52a3124566
djgamertri <djgamertri@gmail.com>
2023-06-08 11:37:10 -0500

    Tabla Usuarios

---
+++ FrontEnd/Assets/UserTable.php
@@ -0,0 +1,47 @@
+<?php 
+include_once("../../Controller/SendData.php");
+
+$data = json_decode(sendDataGet("http://localhost:3001/api/Residents"));
+
+?>
+<section class="home">
+    <div class="text">
+        <div class="Container">
+            <table>
+                <h1 style="text-align: center; margin: 20px">Usuarios</h1>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Tipo de documento</th>
+                        <th>Nombre</th>
+                        <th>Nacimiento</th>
+                        <th>Numero</th>
+                        <th>Email</th>
+                        <th>Casa</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if(!empty($data)){
+                        foreach ($data as $item) {
+                    ?>
+                        <tr>
+                            <td><?php echo $item->IdResident?></td>
+                            <td><?php echo $item->TypeDocumentResident?></td>
+                            <td><?php echo $item->FullnameResident?></td>
+                            <td><?php echo $item->DateBornResident?></td>
+                            <td><?php echo $item->PhoneNumberResident?></td>
+                            <td><?php echo $item->EmailResident?></td>
+                            <td><?php echo $item->NumberHouseResident?></td>
+                            <td class="actions"> <i class='bx bxs-edit edit' ></i> | <i class='bx bxs-trash trash'></i> </td>
+                        </tr>
+                    <?php
+                        }
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
