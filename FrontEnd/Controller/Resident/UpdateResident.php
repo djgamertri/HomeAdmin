c6fb4ceb6c9032922d65fff35adbb61bda0b6a0f
djgamertri <djgamertri@gmail.com>
2023-06-08 15:36:35 -0500

    Controlador de Actualizacion de residentes

--- FrontEnd/Controller/Resident/UpdateResident.php
+++ FrontEnd/Controller/Resident/UpdateResident.php
@@ -1,25 +1,17 @@
 <?php
-
-echo "hola";
-var_dump($_POST);
-
-// call UpdateResident(1013260718,"Andres Fernando Malagon","2005-12-20","Cedula de Ciudadania",3212946031,"andresfernandoxd1591@gmail.com",'Inactivo',175);
-
 include_once "../SendData.php";
 
 $lastPage = $_SERVER['HTTP_REFERER'];
-$id = 1;
 
 if(!empty($_POST)){
-    if ($_POST["NameResident"]) {
+    if ($_POST["Name"]) {
         $data = json_decode(sendDataUpdate("http://localhost:3001/api/Resident?id=$id",$_POST));
-        if($data->NameResident){
+        if($data->Name){
             header("location: $lastPage?complete=true");
         }else{
             header("location: $lastPage?complete=false");
         }
     }
 }
-
 
 ?>
