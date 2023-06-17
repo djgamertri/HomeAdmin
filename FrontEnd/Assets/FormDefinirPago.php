808501b7aa3a596743393bc467ed818df51ce58f
bluaxl <djgamertri@gmail.com>
2023-06-17 08:19:27 -0500

    Arreglamos el formulario y aplicamos el diseño general de estos

--- FrontEnd/Assets/FormDefinirPago.php
+++ FrontEnd/Assets/FormDefinirPago.php
@@ -1,22 +1,29 @@
-<div class="container">
-    <nav></nav>
-    <section>
-        <form action="../../Controller/tax/Tax.php" method="post">
-            <h1>Formulario de estipulación de pagos administrativos</h1>
-            <div>
-                <label for="TaxValue">Digite la nueva cuota para el pago administrativo</label>
-                <input type="number" class="" id="TaxValue" name="TaxValue" placeholder="ej 55000" required autofocus>
+<section class="home">
+    <div class="text">
+        <div class="container">
+            <div class="container-form">
+                <h2>Formulario de estipulación de pagos administrativos</h2>
+                <form action="../../Controller/tax/Tax.php" method="post">
+                    <div>
+                        <label for="TaxValue" class="form-label">Digite la nueva cuota para el pago administrativo</label>
+                        <input type="number" class="form-input" id="TaxValue" name="TaxValue" placeholder="10000" required autofocus>
+                    </div>
+                    <div>
+                        <label for="TaxYear" class="form-label">Digite el año en que va a estar vigente la nueva cuota de administración</label>
+                        <input type="date" class="form-input" id="TaxYear" name="TaxYear" placeholder="Año" required>
+                    </div>
+                    <div class="form-botton-end">
+                        <button type="submit" class="form-buttom-back">Regresar</button>
+                        <button type="submit" class="form-button">Actualizar</button>
+                    </div>
+                    <?php
+                    if (!empty($_GET)) {
+                        echo $_GET["complete"];
+                    }
+                    ?>
+                </form>
             </div>
-            <div>
-                <label for="TaxYear">Digite el año en que va a estar vigente la nueva cuota de administración</label>
-                <input type="date" class="" id="TaxYear" name="TaxYear" placeholder="Año" required>
-            </div>
-            <input type="submit" value="Enviar">
-            <?php 
-            if (!empty($_GET)){
-                echo $_GET["complete"];
-            } 
-            ?>
-        </form>
-    </section>
-</div>
+            <?php include_once("../../Assets/FooterForm.php") ?>
+        </div>
+    </div>
+</section>  
